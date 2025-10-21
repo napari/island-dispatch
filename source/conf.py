1d4b5df3c11f00a2a97aa495c97ecac58a3b72ba
Tim Monko <timmonko@gmail.com>
2025-10-21 12:28:19 -0500

    improve header links

--- source/conf.py
+++ source/conf.py
@@ -17,6 +17,7 @@ extensions = [
     "myst_parser",
     "ablog",
     "sphinx.ext.intersphinx",
+    "sphinx_design",
 ]
 
 templates_path = ['_templates']
@@ -25,7 +26,12 @@ exclude_patterns = [
 ]
 
 # ABlog configuration
-blog_baseurl = "https://napari.org"
+blog_baseurl = "https://napari.org/island-dispatch"
+blog_title = "The Island Dispatch"
+blog_path = "blog"
+fontawesome_included = True
+post_auto_excerpt = 1
+post_date_format = "%B %d, %Y"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -36,15 +42,21 @@ html_logo = "_static/logo.png"
 html_favicon = "_static/logo.png"
 
 html_theme_options = {
-    #"external_links": [{"name": "napari hub", "url": "https://napari-hub.org"}],
+    "external_links": [
+        {"name": "napari.org", "url": "https://napari.org"},
+        {"name": "napari hub", "url": "https://napari-hub.org"},
+    ],
     "github_url": "https://github.com/napari/napari",
     "navbar_start": ["navbar-logo", "navbar-project"],
     "navbar_end": ["navbar-icon-links", "theme-switcher"],
     "navbar_persistent": [],
     "header_links_before_dropdown": 6,
+    "logo": {
+        "link": "https://napari.org",
+    },
     # "secondary_sidebar_items": ["page-toc"],
-    "pygment_light_style": "napari",
-    "pygment_dark_style": "dracula",
+    "pygments_light_style": "napari",
+    "pygments_dark_style": "dracula",
     # "announcement": "https://napari.org/dev/_static/announcement.html",
     "analytics": {
         # The domain you'd like to use for this analytics instance
@@ -52,4 +64,9 @@ html_theme_options = {
         # The analytics script that is served by Plausible
         "plausible_analytics_url": "https://plausible.io/js/plausible.js",
     },
+}
+
+# HTML context for theme
+html_context = {
+    "default_mode": "auto",
 }
--- source/index.md
+++ source/index.md
@@ -1,11 +1,42 @@
 # The Island Dispatch 🏝️
 
-Here's the napari community blog!
+*News, updates, and stories from the napari community*
 
-## All posts
+Welcome to the napari community blog! Here you'll find the latest news about napari development, community initiatives, release announcements, and technical deep dives.
+
+---
+
+## Latest Posts
+
+```{postlist} 5
+   :date: %B %d, %Y
+   :format: **{title}** · *{date}* · by {author}
+   :excerpts:
+   :expand: Continue reading →
+```
+
+---
+
+## Archive
+
+Browse all posts by year:
+
+### 2025
 
 ```{postlist}
-   :list-style: circle
-   :format: "{title}"
-   :sort:
+   :date: %B %d, %Y
+   :format: **{title}** · *{date}* · by {author}
 ```
+
+### Older Posts
+
+For posts from previous years, see the [complete archive](blog/archive.html).
+
+---
+
+## Stay Connected
+
+- 💬 Join the conversation on [Zulip](https://napari.zulipchat.com)
+- 🐙 Follow development on [GitHub](https://github.com/napari/napari)
+- 🌐 Visit the main [napari website](https://napari.org)
+- 🔌 Explore plugins on [napari hub](https://napari-hub.org)
